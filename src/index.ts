9601ed000b80660297288962eb1d020a88c6746f
Dan Forsberg <dforsber@gmail.com>
2025-02-24 19:51:33 +0200

    refine

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,24 @@
-import { WSChannel } from "./lib/isecure.class";
+import { WSChannel } from "./lib/isecure.class.js";
+import { fileURLToPath } from "url";
+import path, { dirname } from "node:path";
 import * as fs from "fs";
 import * as openpgp from "openpgp";
 
-const BaseUrl = "https://ws-api.test.isecure.fi/v2";
-const publicKey = fs.readFileSync("test.pem", "utf8").toString();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
-const publicKeyArmored = fs.readFileSync("./test-pgp-key.pub", "utf8").toString();
-const privateKeyArmored = fs.readFileSync("./test-pgp-key.sec", "utf8").toString();
+const BaseUrl = "https://ws-api.test.isecure.fi/v2";
+const publicKey = fs.readFileSync(path.join(__dirname, "../test/test.pem"), "utf8").toString();
+const publicKeyArmored = fs.readFileSync(path.join(__dirname, "../test/test-pgp-key.pub"), "utf8").toString();
+const privateKeyArmored = fs.readFileSync(path.join(__dirname, "../test/test-pgp-key.sec"), "utf8").toString();
 
 async function createAndVerifyPgpSignature() {
   // Create signature for a file and upload
   const publicKey = await openpgp.readKey({ armoredKey: publicKeyArmored });
   const privateKey = await openpgp.readPrivateKey({ armoredKey: privateKeyArmored });
-  const message = await openpgp.createMessage({ text: fs.readFileSync("testfile.xml", "utf8").toString() });
+  const message = await openpgp.createMessage({
+    text: fs.readFileSync(path.join(__dirname, "../test/testfile.xml"), "utf8").toString(),
+  });
   const detachedSignature = await openpgp.sign({
     message, // Message object
     signingKeys: privateKey,
@@ -65,7 +71,7 @@ async function main() {
   console.log(`Logged in (${ws.props.Mode})`);
 
   const detachedSignature = await createAndVerifyPgpSignature();
-  const contents = fs.readFileSync("testfile.xml", "utf8").toString();
+  const contents = fs.readFileSync(path.join(__dirname, "../test/testfile.xml"), "utf8").toString();
   await ws.uploadFile(Buffer.from(contents).toString("base64"), "testfile.xml", "DUMMY", detachedSignature);
 
   const resp = await ws.listFiles("VKEUR", "ALL");
